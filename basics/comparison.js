ef5452b9c1ce32eaaccaa420d5c7e00690e69715
Barshad Thakuri <mallabarshad93@gmail.com>
2024-07-23 02:19:50 +0000

    changes

---
+++ basics/comparison.js
@@ -0,0 +1,6 @@
+console.log(null >0);
+console.log(null ==0);
+console.log(null >=0);
+// null>=0,  where null is treated as 0 so the value is true
+console.log(undefined==0); 
+console.log("2"===2);
